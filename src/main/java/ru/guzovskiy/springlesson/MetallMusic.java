469d27940e440b2c4b261a767d5ac23c089a290e
Armalys <mashnert@mail.ru>
2020-02-27 16:24:09 +0300

    Lesson - 4

---
+++ src/main/java/ru/guzovskiy/springlesson/MetallMusic.java
@@ -0,0 +1,8 @@
+package ru.guzovskiy.springlesson;
+
+public class MetallMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hard Rock";
+    }
+}
--- src/main/java/ru/guzovskiy/springlesson/MusicPlayer.java
+++ src/main/java/ru/guzovskiy/springlesson/MusicPlayer.java
@@ -1,13 +1,37 @@
 package ru.guzovskiy.springlesson;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MusicPlayer(Music music) {
+public class MusicPlayer {
+    private List<Music> music = new ArrayList<>();
+    private String name;
+    private int volume;
+
+    public void setMusic(List<Music> music) {
         this.music = music;
     }
 
+    public MusicPlayer() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     public void play() {
-        System.out.println("Playing: " + music.getSong());
+        music.forEach(song -> System.out.println("Plaing: " + song.getSong()));
     }
 }
